c54f304650b9a17c929a0aaa41625f7552a0ddec
fsteltenkamp <florian@steltenkamp.de>
2025-02-01 21:55:29 +0100

    prepare routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+include __DIR__.'/web/auth.php';
+include __DIR__.'/web/backend/config.php';
+include __DIR__.'/web/backend/user.php';
+include __DIR__.'/web/frontend/profile.php';
