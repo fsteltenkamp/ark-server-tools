1b43c1faa51f706a39bf5a23349793cf750e6e4e
fsteltenkamp <florian@steltenkamp.de>
2025-03-21 19:37:28 +0100

    add owner to dynamic config

--- app/src/app/Models/User.php
+++ app/src/app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(AseServer::class);
     }
+
+    public function aseDynamicConfigs()
+    {
+        return $this->hasMany(AseDynamicConfig::class);
+    }
 }
