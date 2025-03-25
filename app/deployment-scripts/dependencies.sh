c4163d359ec2ca20da71a73f5c9b32219cab2c72
fsteltenkamp <florian@steltenkamp.de>
2025-03-25 11:49:28 +0100

    try adding deployment scripts

---
+++ app/deployment-scripts/dependencies.sh
@@ -0,0 +1,12 @@
+cd /app
+rm -rf vendor node_modules
+composer install --no-dev --optimize-autoloader
+npm install
+npm run build
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+if [ -f artisan ]; then
+    php artisan key:generate
+    php artisan migrate
+fi
