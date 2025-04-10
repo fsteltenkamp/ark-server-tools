acf496be63f2793a6c43ab8c8092d909e014dc4c
fsteltenkamp <florian@steltenkamp.de>
2025-04-10 19:27:13 +0200

    create .env if it doesnt exist

--- app/deployment-scripts/dependencies.sh
+++ app/deployment-scripts/dependencies.sh
@@ -5,6 +5,11 @@ rm -rf vendor node_modules
 composer install --no-dev --optimize-autoloader
 npm install
 npm run build
+
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
 if [ -f artisan ]; then
     php artisan key:generate
     php artisan migrate
