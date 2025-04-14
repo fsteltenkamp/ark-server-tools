23c5490c3a9b4265ca95f64d7502ab4824e7300a
fsteltenkamp <florian@steltenkamp.de>
2025-04-14 15:35:36 +0200

    messages

--- app/deployment-scripts/00.dependencies.sh
+++ app/deployment-scripts/00.dependencies.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 cd /app
+
+echo "================================================"
+echo "         Removing vendor and node_modules"
 rm -rf vendor node_modules
+echo "================================================"
+
+echo "            Installing dependencies"
 composer install --no-dev --optimize-autoloader
 npm install
 npm run build
+echo "================================================"
 
 if [ ! -f .env ]; then
+    echo "          Creating .env file"
     cp .env.example .env
+    echo "==============================================="
 fi
 
 if [ -f artisan ]; then
+    echo "        Running Laravel Setup Commands"
     php artisan key:generate
     php artisan migrate
+    echo "==============================================="
 fi
