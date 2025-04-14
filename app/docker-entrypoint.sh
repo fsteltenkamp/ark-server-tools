5647415f2c2dbf166808c73e6fe5e372a632d1de
fsteltenkamp <florian@steltenkamp.de>
2025-04-14 16:12:52 +0200

    try to overhaul waiting for init scripts to finish...

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -3,11 +3,18 @@
 ls /docker-entrypoint-init.d/ > /dev/null
 for file in /docker-entrypoint-init.d/*; do
     if [ -f "$file" ]; then
-        echo "Running $file"
-        chmod +x "$file"  # Ensure the file is executable
-        "$file"           # Execute the file directly
+        # get only the file name without path:
+        filename=$(basename "$file")
+        echo "Running $filename"
+        # Ensure the file is executable
+        chmod +x "$file"
+        # make logs directory:
+        mkdir -p /docker-entrypoint-init.logs/"$filename"
+        # Execute the file directly
+        "$file" > /docker-entrypoint-init.logs/"$filename"/$(date "+%Y%m%d-%H%M%S").log
         # wait until the file is finished
         while [ -d /proc/$(pgrep -f "$file") ]; do
+            echo "."
             sleep 1
         done
     fi
